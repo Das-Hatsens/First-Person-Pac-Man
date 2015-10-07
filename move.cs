4fea1ab7aee72be0c2f0cd37e9397caeff56f5a6
Scott Waye <scott.waye@hubse.com>
2015-10-07 14:38:00 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/CurrentLayout.dwlt
    	Library/InspectorExpandedItems.asset
    	Library/assetDatabase3
    	Library/metadata/00/00000000000000002000000000000000
    	Library/metadata/00/00000000000000003000000000000000
    	Library/metadata/00/00000000000000004000000000000000
    	Library/metadata/00/00000000000000004100000000000000
    	Library/metadata/00/00000000000000005000000000000000
    	Library/metadata/00/00000000000000005100000000000000
    	Library/metadata/00/00000000000000006000000000000000
    	Library/metadata/00/00000000000000006100000000000000
    	Library/metadata/00/00000000000000007000000000000000
    	Library/metadata/00/00000000000000008000000000000000
    	Library/metadata/00/00000000000000008100000000000000
    	Library/metadata/00/00000000000000009000000000000000
    	Library/metadata/00/00000000000000009100000000000000
    	Library/metadata/00/0000000000000000a000000000000000
    	Library/metadata/00/0000000000000000b000000000000000
    	Library/metadata/00/0000000000000000c000000000000000
    	Library/metadata/5f/5f32cd94baa94578a686d4b9d6b660f7
    	Library/metadata/73/739bbd9f364b4268874f9fd86ab3beef
    	Library/metadata/80/80a3616ca19596e4da0f10f14d241e9f
    	Library/metadata/85/852e56802eb941638acbb491814497b0
    	Library/metadata/87/870353891bb340e2b2a9c8707e7419ba
    	Library/metadata/97/97decbdab0634cdd991f8d23ddf0dead
    	Library/metadata/f5/f5f67c52d1564df4a8936ccd202a3bd8
    	Library/shadercompiler-32bit-1.log
    	Library/shadercompiler-64bit-1.log

---
+++ move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z +.3f);
+
+    }
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+            Debug.Log("Detected a keyboard event!");
+
+    }
+}
