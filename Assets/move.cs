e9f2ac046ef9f2512bc89e42c9875db741b959ec
Scott Waye <scott.waye@hubse.com>
2015-10-08 20:51:49 -0500

    add movement

---
+++ Assets/move.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+    float speed = 0.05f;
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        direction = new Vector3(0, 0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + direction;
+    }
+    void OnGUI()    {        Event e = Event.current;
+        
+        if (e.type == EventType.KeyDown && e.isKey)
+        {
+            if (e.keyCode == KeyCode.LeftArrow)
+            {
+                direction =  Quaternion.Euler(0, -90, 0) * direction;
+                transform.Rotate(0, -90, 0);
+            }
+            else if (e.keyCode == KeyCode.RightArrow)
+            {
+                direction =  Quaternion.Euler(0, 90, 0) * direction;
+                transform.Rotate(0, 90, 0);
+            }
+        }
+    }
+}
