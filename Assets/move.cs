8935c0ea15dabc8ad0984483b2fd53eead78ddec
Scott Waye <scott.waye@hubse.com>
2015-10-08 21:17:58 -0500

    add slow rotation

--- Assets/move.cs
+++ Assets/move.cs
@@ -24,13 +24,32 @@ public class move : MonoBehaviour
             if (e.keyCode == KeyCode.LeftArrow)
             {
                 direction =  Quaternion.Euler(0, -90, 0) * direction;
-                transform.Rotate(0, -90, 0);
+                StartCoroutine(RotateObject(transform, new Vector3(0, -90, 0), 0.5f));
             }
             else if (e.keyCode == KeyCode.RightArrow)
             {
                 direction =  Quaternion.Euler(0, 90, 0) * direction;
-                transform.Rotate(0, 90, 0);
+                StartCoroutine(RotateObject(transform, new Vector3(0, 90, 0), 0.5f));
             }
         }
+    }
+
+    bool rotating;
+
+    IEnumerator RotateObject(Transform transform, Vector3 degrees , float seconds)
+    {
+        if (rotating)  yield return null;
+        rotating = true;
+
+        var startRotation = transform.rotation;
+        var endRotation = transform.rotation * Quaternion.Euler(degrees);
+        var t = 0.0f;
+        float rate = 1.0f / seconds;
+        while (t < 1.0) {
+            t += Time.deltaTime * rate;
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+            yield return null;
+        }
+        rotating = false;
     }
 }
